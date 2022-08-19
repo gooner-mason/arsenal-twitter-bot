5535cc43138a3b8779ffedc7ba84795a495651ee
Mason Maier <masonjmaier@gmail.com>
2022-08-19 08:54:24 -0700

    fixed error

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require("express")
 
-const { jobAt4, jobAt6, jobAt10, jobAt8, jobAt12 } = require("./src/twitterBotJob.js");
-// const { test } = require("./src/test.js");
+const { jobAt4, jobAt6, jobAt10, jobAt8, jobAt12, jobAt2 } = require("./src/twitterBotJob.js");
 
 const app = express();
 
 app.get("/", (req, res) => {
 	res.send("Check out @Gooner_Bot on Twitter")
-	// test.start()
 	jobAt4.start()
 	jobAt6.start()
 	jobAt8.start()
 	jobAt10.start()
 	jobAt12.start()
+	jobAt2.start()
 })
 
 app.listen(process.env.PORT || 3000, () => {
--- src/twitterBotJob.js
+++ src/twitterBotJob.js
@@ -11,8 +11,6 @@ const getFixture = async () => {
 	try {
 		// GET requests to RapidAPI
 		const fixtures = await axios.get("https://v3.football.api-sports.io/fixtures?season=2022&team=42&next=2", {"headers" : headers})
-		const fixtureId = fixtures.data.response[0].fixture.id
-		const predictions = await axios.get(`https://v3.football.api-sports.io/predictions?fixture=${fixtureId}`, {"headers" : headers})
 		
 		// days til match
 		const countDownDate = new Date(fixtures.data.response[0].fixture.date).getTime()
@@ -22,10 +20,15 @@ const getFixture = async () => {
 		const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
 		const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
 
+		if (days < 5) {
 		const opponent = (fixtures.data.response[0].teams.home.name.includes("Arsenal") 
 		? fixtures.data.response[0].teams.away.name : fixtures.data.response[0].teams.home.name)
 		const stadium = fixtures.data.response[0].fixture.venue.name
 		const city = fixtures.data.response[0].fixture.venue.city
+
+		const fixtureId = fixtures.data.response[0].fixture.id
+		const predictions = await axios.get(`https://v3.football.api-sports.io/predictions?fixture=${fixtureId}`, {"headers" : headers})
+
 		const leagueName= predictions.data.response[0].league.name
 		const arsenalForm = (predictions.data.response[0].teams.home.name.includes("Arsenal") 
 		? predictions.data.response[0].teams.home.league.form : predictions.data.response[0].teams.away.league.form)
@@ -34,29 +37,18 @@ const getFixture = async () => {
 		// tweet messages
 		const content1 = 
 		`⚽ INFORMATION ⚽
-		🔴 Arsenal face ${opponent} 🔴
-		⚪ Kickoff In ${days} day(s) ${hours} hr(s) ${minutes} min(s) ⚪
+		🔴 Opponent: ${opponent} 🔴
+		⚪ Kickoff: ${days} day(s) ${hours} hr(s) ${minutes} min(s) ⚪
 		🔴 Stadium: ${stadium} 🔴 
-		⚪ Location: ${city} ⚪ 
 		🔴 League: ${leagueName} 🔴 
 		⚪ Current Form: ${arsenalFormNull} ⚪ 
-		#arsenal #afc #coyg #aresenalPicks #arsenalBets #sportsBetting #freePicks #goonerBot`
-		const content3 = 
-		`⚽ INFORMATION ⚽
-		🔴 Arsenal face ${opponent}... countdown is work in progress🔴
-		⚪ Stadium: ${stadium} ⚪
-		🔴 Location: ${city} 🔴
-		⚪ League: ${leagueName} ⚪
-		🔴 Current Form: ${arsenalFormNull} 🔴
-		#arsenal #afc #coyg #aresenalPicks #arsenalBets #sportsBetting #freePicks #goonerBot`
-		
+		#arsenal #afc #coyg #aresenalPicks #arsenalBets #freePicks`
+	
 		// send INFORMATION tweet 
 		rwClient.v2.tweet(content1)
-		rwClient.v2.tweet(content3)
-		// console.log(content1);
 		
 		// send PREDICTION tweet
-		if (days <= 0 && hours <= 2) {
+		if (days === 0 && hours <=2 ) {
 		const winner = predictions.data.response[0].predictions.winner.name === null ? "n/a" 
 		: predictions.data.response[0].predictions.winner.name
 		const winOrDraw = predictions.data.response[0].predictions.win_or_draw === true ? "Yes" : "No"
@@ -64,7 +56,6 @@ const getFixture = async () => {
 		? "n/a" : predictions.data.response[0].predictions.under_over.includes("-") 
         ? predictions.data.response[0].predictions.under_over.replace("-", "U") 
         : predictions.data.response[0].predictions.under_over.replace("+", "O")
-		// const advice = predictions.data.response[0].predictions.advice
 		const arsenalGoals = predictions.data.response[0].teams.home.name.toLowerCase() === "arsenal" 
 		? predictions.data.response[0].predictions.goals.home 
 		: predictions.data.response[0].predictions.goals.away
@@ -72,10 +63,7 @@ const getFixture = async () => {
 		const arsenalGoalsOU = arsenalGoals === null ? "n/a" : arsenalGoals.includes("-") 
 		? arsenalGoals.replace("-", "U") : arsenalGoals.replace("+", "O")
 
-		const opponent = (fixtures.data.response[0].teams.home.name.includes("Arsenal") 
-		? fixtures.data.response[0].teams.away.name : fixtures.data.response[0].teams.home.name)
-
-		const opponentGoals = predictions.data.response[0].teams.home.name.toLowerCase() === `${opponent}`
+		const opponentGoals = predictions.data.response[0].teams.home.name === `${opponent}`
 		? predictions.data.response[0].predictions.goals.home 
 		: predictions.data.response[0].predictions.goals.away 
 
@@ -86,15 +74,13 @@ const getFixture = async () => {
 		`⚽ PREDICTIONS ⚽
 		🔴 Winner: ${winner} 🔴
 		⚪ Draw No Bet: ${winOrDraw} ⚪
-		#arsenal #afc #coyg #aresenalPicks #arsenalBets #sportsBetting #freePicks #goonerBot`
+		⚪ Arsenal Goals: ${arsenalGoalsOU} ⚪
+		🔴 ${opponent} Goals: ${opponentGoalsOU} 🔴
+		#arsenal #afc #coyg #aresenalPicks #arsenalBets #freePicks`
 		
-		// 🔴 Over/Under: ${overUnder} 🔴
-		// ⚪ Arsenal Goals: ${arsenalGoalsOU} ⚪
-		// 🔴 ${opponent} Goals: ${opponentGoalsOU} 🔴
 		rwClient.v2.tweet(content2)
-		// console.log(content2);
 	}
-
+}
 	} catch (error) {
 		console.log(error)
 	}
@@ -129,5 +115,10 @@ const getFixture = async () => {
 		await getFixture()
 		console.log("Successfully sent a tweet at: " + new Date())
 }) 	
+	// 12pm MST
+	const jobAt2 = cron.schedule("0 21 * * *", async () => {
+		await getFixture()
+		console.log("Successfully sent a tweet at: " + new Date())
+}) 	
 
-module.exports = { jobAt4, jobAt6, jobAt8, jobAt10, jobAt12 }
+module.exports = { jobAt4, jobAt6, jobAt8, jobAt10, jobAt12, jobAt2 }
