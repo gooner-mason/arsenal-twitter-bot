3fc6be8996e58c43bd47602e8820c70bd0e44ad4
Mason Maier <72817616+masonjmaier23@users.noreply.github.com>
2022-10-02 17:20:07 -0700

    Update app.js
    Added job

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express")
 
-const { jobAt4, jobAt6, jobAt10, jobAt8, jobAt12, jobAt2 } = require("./src/twitterBotJob.js");
+const { jobAt2am, jobAt4, jobAt6, jobAt10, jobAt8, jobAt12, jobAt2pm } = require("./src/twitterBotJob.js");
 
 const app = express();
 
 app.get("/", (req, res) => {
 	res.send("Check out @Gooner_Bot on Twitter")
+        jobAt2am.start()
 	jobAt4.start()
 	jobAt6.start()
 	jobAt8.start()
 	jobAt10.start()
 	jobAt12.start()
-	jobAt2.start()
+	jobAt2pm.start()
 })
 
 app.listen(process.env.PORT || 3000, () => {
