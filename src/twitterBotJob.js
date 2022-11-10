d1f44b484ba8bee46903e56db85a1db8c23eb959
Mason Maier <masonjmaier@gmail.com>
2022-11-10 08:43:29 -0700

    refactored

--- src/twitterBotJob.js
+++ src/twitterBotJob.js
@@ -10,105 +10,103 @@ const headers = {
 	"x-rapidapi-host": process.env.X_RAPIDAPI_HOST
 };
 
-const getFixture = async () => {
-	try {
-		// GET requests to RapidAPI for fixture data
-		const fixtures = await axios.get("https://v3.football.api-sports.io/fixtures?season=2022&team=42&next=2", {"headers" : headers});
-		
-		// days til match
-		const countDownDate = new Date(fixtures.data.response[0].fixture.date).getTime();
-		const now = new Date().getTime();
-		const timeLeft = countDownDate - now;
-		const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-		const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-		// if less than 5 days til match
-		if (days < 5) {
-		
-			// get opponent 
-			const opponent = (fixtures.data.response[0].teams.home.name.includes("Arsenal") 
-				? fixtures.data.response[0].teams.away.name : fixtures.data.response[0].teams.home.name);
-		
-			// get stadium
-			const stadium = fixtures.data.response[0].fixture.venue.name;
-
-			// get fixtureId and GET requests to RapidAPI for prediction data
-			const fixtureId = fixtures.data.response[0].fixture.id;
-			const predictions = await axios.get(`https://v3.football.api-sports.io/predictions?fixture=${fixtureId}`, {"headers" : headers});
-
-			// get league name
-			const leagueName= predictions.data.response[0].league.name;
-
-			// get arsenal form
-			const arsenalForm = (predictions.data.response[0].teams.home.name.includes("Arsenal") 
-				? predictions.data.response[0].teams.home.league.form : predictions.data.response[0].teams.away.league.form);
-			const arsenalFormNull = arsenalForm === null ? "n/a" : arsenalForm;
-		
-			// information tweet
-			const content1Days = 
-			`⚽ Arsenal face ${opponent} in ${days} day(s) ⚽
-			🔴 Stadium: ${stadium} 🔴 
-			⚪ League: ${leagueName} ⚪
-			🔴 Current Form: ${arsenalFormNull} 🔴
-			#arsenal #afc #coyg`;
-		
-			// information tweet
-			const content1Hours = 
-			`⚽ Arsenal face ${opponent} in ${hours} hr(s) ⚽
-			🔴 Stadium: ${stadium} 🔴 
-			⚪ League: ${leagueName} ⚪
-			🔴 Current Form: ${arsenalFormNull} 🔴
-			#arsenal #afc #coyg`;
+async function getMatchData() {
+	// GET requests to RapidAPI for fixture data
+	const fixtures = await axios.get("https://v3.football.api-sports.io/fixtures?season=2022&team=42&next=2", {"headers" : headers});
 	
-			// send information tweet if days is > 0
-			if (days > 0) {
-				rwClient.v2.tweet(content1Days);
-			} 
+	// days til match
+	const countDownDate = new Date(fixtures.data.response[0].fixture.date).getTime();
+	const now = new Date().getTime();
+	const timeLeft = countDownDate - now;
+	const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-			if (days === 0 ) {
-				rwClient.v2.tweet(content1Hours);
-			}
-		
-			// if days til match is 0 and less than or equal to 2 hours til match
-			if (days < 1) {
-			// get predicted winner
-				const winner = predictions.data.response[0].predictions.winner.name === null ? "n/a" 
-					: predictions.data.response[0].predictions.winner.name;
-		
-				// get predicted arsenal goals
-				const arsenalGoals = predictions.data.response[0].teams.home.name.toLowerCase() === "arsenal" 
-					? predictions.data.response[0].predictions.goals.home 
-					: predictions.data.response[0].predictions.goals.away;
-				const arsenalGoalsOU = arsenalGoals === null ? "n/a" : arsenalGoals.includes("-") 
-					? arsenalGoals.replace("-", "U") : arsenalGoals.replace("+", "O");
-		
-				// get predicted opponent goals
-				const opponentGoals = predictions.data.response[0].teams.home.name === `${opponent}`
-					? predictions.data.response[0].predictions.goals.home 
-					: predictions.data.response[0].predictions.goals.away; 
-				const opponentGoalsOU = opponentGoals === null ? "n/a" : opponentGoals.includes("-")
-					? opponentGoals.replace("-", "U") : opponentGoals.replace("+", "O");
+	// get opponent 
+	const opponent = (fixtures.data.response[0].teams.home.name.includes("Arsenal") 
+		? fixtures.data.response[0].teams.away.name : fixtures.data.response[0].teams.home.name);
 
-				// predictions tweet
-				const content2 = 
-		`⚽ Predictions against ${opponent} ⚽
-		🔴 Winner: ${winner} 🔴
-		⚪️ Arsenal Goals: ${arsenalGoalsOU} ⚪️
-		🔴 ${opponent} Goals: ${opponentGoalsOU} 🔴
-		#arsenal #afc #coyg`;
-				
-				// send predictions tweet
-				rwClient.v2.tweet(content2);
-			}
+	// get stadium
+	const stadium = fixtures.data.response[0].fixture.venue.name;
+
+	// get fixtureId and GET requests to RapidAPI for prediction data
+	const fixtureId = fixtures.data.response[0].fixture.id;
+	const predictions = await axios.get(`https://v3.football.api-sports.io/predictions?fixture=${fixtureId}`, {"headers" : headers});
+
+	// get league name
+	const leagueName= predictions.data.response[0].league.name;
+
+	// get arsenal form
+	const arsenalForm = (predictions.data.response[0].teams.home.name.includes("Arsenal") 
+		? predictions.data.response[0].teams.home.league.form : predictions.data.response[0].teams.away.league.form);
+	const arsenalFormNull = arsenalForm === null ? "n/a" : arsenalForm;
+
+	const winner = predictions.data.response[0].predictions.winner.name === null ? "n/a" 
+		: predictions.data.response[0].predictions.winner.name;
+
+	// get predicted arsenal goals
+	const arsenalGoals = predictions.data.response[0].teams.home.name.toLowerCase() === "arsenal" 
+		? predictions.data.response[0].predictions.goals.home 
+		: predictions.data.response[0].predictions.goals.away;
+	const arsenalGoalsOU = arsenalGoals === null ? "n/a" : arsenalGoals.includes("-") 
+		? arsenalGoals.replace("-", "U") : arsenalGoals.replace("+", "O");
+
+	// get predicted opponent goals
+	const opponentGoals = predictions.data.response[0].teams.home.name === `${opponent}`
+		? predictions.data.response[0].predictions.goals.home 
+		: predictions.data.response[0].predictions.goals.away; 
+	const opponentGoalsOU = opponentGoals === null ? "n/a" : opponentGoals.includes("-")
+		? opponentGoals.replace("-", "U") : opponentGoals.replace("+", "O");
+
+	const matchData = { days, hours, opponent, stadium, leagueName, arsenalFormNull, winner, arsenalGoalsOU, opponentGoalsOU };
+	
+	console.log(matchData);
+}
+
+async function sendTweet() {
+	const matchData = await getMatchData();
+
+	// information tweet >0 days
+	const infoTweetDays = 
+	`⚽ Arsenal face ${matchData.opponent} in ${matchData.days} day(s) ⚽
+	🔴 Stadium: ${matchData.stadium} 🔴 
+	⚪ League: ${matchData.leagueName} ⚪
+	🔴 Current Form: ${matchData.arsenalFormNull} 🔴
+	#arsenal #afc #coyg`;
+
+	// information tweet <1 days
+	const infoTweetHours = 
+	`⚽ Arsenal face ${matchData.opponent} in ${matchData.hours} hr(s) ⚽
+	🔴 Stadium: ${matchData.stadium} 🔴 
+	⚪ League: ${matchData.leagueName} ⚪
+	🔴 Current Form: ${matchData.arsenalFormNull} 🔴
+	#arsenal #afc #coyg`;
+
+	// prediction tweet
+	const predictionTweet = 
+	`⚽ Predictions against ${matchData.opponent} ⚽
+	🔴 Winner: ${matchData.winner} 🔴
+	⚪️ Arsenal Goals: ${matchData.arsenalGoalsOU} ⚪️
+	🔴 ${matchData.opponent} Goals: ${matchData.opponentGoalsOU} 🔴
+	#arsenal #afc #coyg`;
+
+	if (matchData.days < 5) {
+		// send information tweet if days is > 0
+		if (matchData.days > 0) {
+			rwClient.v2.tweet(infoTweetDays);
+		} 
+		if (matchData.days === 0 ) {
+			rwClient.v2.tweet(infoTweetHours);
 		}
-	} catch (error) {
-		console.log(error);
+		// if days til match is 0 and less than or equal to 2 hours til match
+		if (matchData.days < 1) {
+			rwClient.v2.tweet(predictionTweet);
+		}
 	}
-};
+}
 
 // 2am MST
 const jobAt2am = cron.schedule("0 9 * * *", async () => {
-	await getFixture();
+	await sendTweet();
 	console.log("Successfully sent a tweet at: " + new Date());
 });
 
